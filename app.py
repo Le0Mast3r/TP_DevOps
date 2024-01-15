a8cce22ec919c8cb5305f38bcc673c3069051b1c
Ayoub El Bouad <a.elbouad@nibelis.com>
2024-01-15 13:33:44 +0100

    TP_ETAP_1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import pandas as pd 
+import numpy as np
+
+
+# app = Flask(__name__)
+# @app.route('/')
+# def hello_world():
+# 	return 'pandas dispo'
+
+# if __name__ == '__main__':
+# 	app.run(host='0.0.0.0',port=80)
+
+print("pandas dispo")
