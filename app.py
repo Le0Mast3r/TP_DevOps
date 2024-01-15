f2368e0380eaf5637b060fa85e4e8e4908162459
Ayoub El Bouad <a.elbouad@nibelis.com>
2024-01-15 14:17:31 +0100

    CMT

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask
 import pandas as pd 
 import numpy as np
 
+app = Flask(__name__)
 
-# app = Flask(__name__)
-# @app.route('/')
-# def hello_world():
-# 	return 'pandas dispo'
+@app.route('/')
+def hello_world():
+    return 'pandas dispo'
 
-# if __name__ == '__main__':
-# 	app.run(host='0.0.0.0',port=80)
-
-print("pandas dispo")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
